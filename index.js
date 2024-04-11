0edc2f701fa5ffb72d426c3c35aef9073ffb1e9a
ClementMadiot <clementmadiot09@gmail.com>
2024-04-11 14:36:43 +0700

    add fonction random number

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const messageEl = document.getElementById('message-el')
 const sumEl = document.getElementById('sum-el')
 const cardEl = document.getElementById('card-el')
 ///Cards
-let firstCard = 11
-let secondCard = 7
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
 let cards = [firstCard, secondCard]
 let sum = firstCard + secondCard
 
@@ -12,6 +12,13 @@ let sum = firstCard + secondCard
 let blackjack = false
 let isAlive = true
 let message = ''
+
+///Random card
+
+function getRandomCard() {
+  let randomNumber = Math.floor(Math.random() * 11) + 1
+  return randomNumber
+}
 
 /// Onclick
 function startGame() {
@@ -23,7 +30,6 @@ function renderGame() {
   cardEl.textContent = 'Cards :'
   for (let i = 0; i < cards.length; i++) {
     cardEl.textContent += ' ' + cards[i]
-    console.log(cards[i])
   }
   sumEl.textContent = 'Sum : ' + sum
 
@@ -41,7 +47,7 @@ function renderGame() {
 
 function newCard() {
   console.log('Drawing a new card from the deck')
-  let card = 3
+  let card = getRandomCard()
   sum += card
   cards.push(card)
 
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
-let num1 = 1
-let num2 = 2
-let allNum = [ num1, num2 ]
 
-for (let i = 0; i < allNum.length; i++) {
-  console.log(allNum[i])
-  cardEl.textContent += ' ' + allNum[i]
+function rollDice(){
+  let randomNumber = Math.floor( Math.random() * 6) + 1
+  return randomNumber
+  
 }
+console.log(rollDice())
+
