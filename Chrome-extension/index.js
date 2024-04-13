6c2b8584724eadd7f91a18b503403d7c61455d68
ClementMadiot <clementmadiot09@gmail.com>
2024-04-13 15:13:07 +0700

    add style + renderLeads function with link

---
+++ Chrome-extension/index.js
@@ -0,0 +1,22 @@
+// input element
+const inputEl = document.getElementById('inputEl')
+const inputBtn = document.getElementById('inputBtn')
+// list Element
+const ulEl = document.getElementById('ulEl')
+let myLeads = []
+
+inputBtn.addEventListener('click', (e) => {
+  myLeads.push(inputEl.value)
+  inputEl.value = ''
+  renderLeads()
+})
+
+function renderLeads() {
+  let listItem = ''
+  for (let i = 0; i < myLeads.length; i++) {
+    listItem += `<li> <a href='${myLeads[i]}' target='_blank'>${myLeads[i]} </a></li> `
+    console.log(listItem)
+  }
+
+  ulEl.innerHTML += listItem
+}
