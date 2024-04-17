f9ca58e29694d502b7f2c6667c1202156e108bb6
ClementMadiot <clementmadiot09@gmail.com>
2024-04-17 15:27:27 +0700

    add delete btn / delete dom link and clear localStorage

--- Chrome-extension/index.js
+++ Chrome-extension/index.js
@@ -5,37 +5,41 @@ const inputBtn = document.getElementById('inputBtn')
 const ulEl = document.getElementById('ulEl')
 let myLeads = []
 
-let leadFromLocalStorage = JSON.parse(localStorage.getItem("myLeads", myLeads))
+let leadFromLocalStorage = JSON.parse(localStorage.getItem('myLeads', myLeads))
 
-if(leadFromLocalStorage){
+if (leadFromLocalStorage) {
   myLeads = leadFromLocalStorage
   renderLeads()
 } else {
-  console.log("storage is empty")
+  console.log('storage is empty')
 }
-
-
 
 ///Evenement onClick
 inputBtn.addEventListener('click', (e) => {
-  myLeads.push(inputEl.value)
-  inputEl.value = ''
-  localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
-  
-  renderLeads()
-
+  let inputValue = inputEl.value
+  if (inputValue) {
+    myLeads.push(inputValue)
+    inputValue = ''
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    renderLeads()
+  }
 })
+
+deleteBtn.addEventListener('click', () => {
+  myLeads = localStorage.clear()
+  myLeads = []
+  ulEl.innerHTML = ``
+})
+
 
 function renderLeads() {
   let listItem = ''
-  
   for (let i = 0; i < myLeads.length; i++) {
     listItem += `<li> 
     <a href='${myLeads[i]}' target='_blank'>${myLeads[i]} </a>
     </li> `
     // console.log(listItem)
   }
-
   ulEl.innerHTML = listItem
 }
 
