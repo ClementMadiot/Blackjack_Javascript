cefc4f2747656809e26863f9be902a1ebe683d99
ClementMadiot <clementmadiot09@gmail.com>
2024-04-18 16:12:50 +0700

    add save tab btn and his evenment on click

--- Chrome-extension/index.js
+++ Chrome-extension/index.js
@@ -1,19 +1,40 @@
 // input element
 const inputEl = document.getElementById('inputEl')
 const inputBtn = document.getElementById('inputBtn')
+const tabBtn = document.getElementById('tabBtn')
 // list Element
 const ulEl = document.getElementById('ulEl')
+//Storage Array
 let myLeads = []
-
 let leadFromLocalStorage = JSON.parse(localStorage.getItem('myLeads', myLeads))
 
 if (leadFromLocalStorage) {
   myLeads = leadFromLocalStorage
-  renderLeads()
-} else {
-  console.log('storage is empty')
+  render(myLeads)
 }
 
+const tabs = [{ url: 'https://www.google.com/' }]
+
+tabBtn.addEventListener('click', () => {
+
+
+  let tabValue = tabs[0].url
+  myLeads.push(tabValue)
+  tabValue = ''
+  localStorage.setItem('myLeads', JSON.stringify(myLeads))
+  render(myLeads)
+})
+
+function render(leads) {
+  let listItem = ''
+  for (let i = 0; i < leads.length; i++) {
+    listItem += `<li> 
+    <a href='${leads[i]}' target='_blank'>${leads[i]} </a>
+    </li> `
+    // console.log(listItem)
+  }
+  ulEl.innerHTML = listItem
+}
 ///Evenement onClick
 inputBtn.addEventListener('click', (e) => {
   let inputValue = inputEl.value
@@ -21,25 +42,12 @@ inputBtn.addEventListener('click', (e) => {
     myLeads.push(inputValue)
     inputValue = ''
     localStorage.setItem('myLeads', JSON.stringify(myLeads))
-    renderLeads()
+    render(myLeads)
   }
 })
 
 deleteBtn.addEventListener('click', () => {
-  myLeads = localStorage.clear()
+  localStorage.clear()
   myLeads = []
-  ulEl.innerHTML = ``
+  render(myLeads)
 })
-
-
-function renderLeads() {
-  let listItem = ''
-  for (let i = 0; i < myLeads.length; i++) {
-    listItem += `<li> 
-    <a href='${myLeads[i]}' target='_blank'>${myLeads[i]} </a>
-    </li> `
-    // console.log(listItem)
-  }
-  ulEl.innerHTML = listItem
-}
-
