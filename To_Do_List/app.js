1a4c05b5abc67597cb5365c7e909edc1f456cddd
ClementMadiot <clementmadiot09@gmail.com>
2024-05-01 16:52:24 +0700

    display value + bnt clear + alert

--- To_Do_List/app.js
+++ To_Do_List/app.js
@@ -16,6 +16,9 @@ let editID = ''
 //: submit form
 form.addEventListener('submit', addItem)
 
+//: clear items
+clearBtn.addEventListener('click', clearItems)
+
 // ****** FUNCTIONS **********
 function addItem(e) {
   e.preventDefault()
@@ -25,9 +28,34 @@ function addItem(e) {
   }
   //! not good practice to have a unique number
   const id = new Date().getTime().toString()
+
   /// if value is true and editFlag is false
   if (value && !editFlag) {
-    console.log('add item')
+    /// add class and id
+    const element = document.createElement('article')
+    element.classList.add('grocery-item')
+    const attr = document.createAttribute('data-id')
+    attr.value = id
+    element.setAttributeNode(attr)
+    element.innerHTML = `
+          <p class="title">${value}</p>
+          <div class="btn-container">
+            <button class="edit-btn">
+              <i class="fas fa-edit"></i>
+            </button>
+            <button class="delete-btn">
+              <i class="fas fa-trash"></i>
+            </button>
+          </div>`
+    list.appendChild(element)
+
+    //: display the alert
+    displayAlert('item added to the list', 'succes')
+    container.classList.add('show-container')
+    //: add localStorage and set back to default
+    addToLocalStorage(id, value)
+    setBackToDefault()
+
     /// if value is empty and editFlag is false
   } else if (value && editFlag) {
     console.log('editing')
@@ -35,17 +63,39 @@ function addItem(e) {
     displayAlert('please enter value', 'danger')
   }
 }
+
 //: Display alert
 function displayAlert(text, action) {
   alert.textContent = text
   alert.classList.add(`alert-${action}`)
 
-//: Remove alert
-setTimeout(() => {
-  alert.textContent = ''
-  alert.classList.remove(`alert-${action}`)
-}, 1000);
+  //: Remove alert
+  setTimeout(() => {
+    alert.textContent = ''
+    alert.classList.remove(`alert-${action}`)
+  }, 1000)
+}
+//: Clear Items
+function clearItems() {
+  const items = document.querySelectorAll('.grocery-item')
+  if (items.length > 0) {
+    items.forEach(function (item) {
+      list.removeChild(item)
+    })
+  }
+  container.classList.remove('show-container')
+  displayAlert('empty list', 'danger')
+}
+
+// ****** SET BACK TO DEFAULT **********
+function setBackToDefault() {
+  grocery.value = ''
+  editFlag = false
+  editID = ''
+  submitBtn.textContent = 'submit'
 }
 // ****** LOCAL STORAGE **********
-
+function addToLocalStorage(id, value) {
+  console.log('add')
+}
 // ****** SETUP ITEMS **********
